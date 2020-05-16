74890ce8893b597cd74524eb4d8a5608924d7d85
ahmedHazem-iti <ahmed_hazem_refaie@hotmail.com>
2020-05-17 00:09:33 +0200

    solve mashakel yassmin

--- database/migrations/2020_05_16_173827_create_quizzes_table.php
+++ database/migrations/2020_05_16_173827_create_quizzes_table.php
@@ -23,8 +23,10 @@ class CreateQuizzesTable extends Migration
             $table->string('contactTybe')->nullable();
             $table->string('customerName')->nullable();
             $table->string('customerPhoneNo')->nullable();
+
             $table->unsignedBigInteger('category_id')->nullable();
             $table->foreign('design_id')->references('id')->on('designs');
+
             $table->softDeletes();
             $table->timestamps();
         });
