d510db76fe59acc31700c7621700797193cd0541
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-05-22 10:05:58 +0200

    Edite Design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,6 @@
 
 @section('content')
 
-    @component('components.header')
-    @endcomponent
-
-    @component('components.projects')
-    @endcomponent
-
-    @component('components.statitics')
-    @endcomponent
-
-    @component('components.services')
-    @endcomponent
-
-    @component('components.all_projects')
-    @endcomponent
-
-    @component('components.information')
-    @endcomponent
 
     @component('components.state')
     @endcomponent
@@ -29,15 +12,6 @@
     @component('components.details')
     @endcomponent
     @component('components.worldWide')
-    @endcomponent
-
-    @component('components.testimonials')
-    @endcomponent
-
-    @component('components.skills')
-    @endcomponent
-
-    @component('components.clients')
     @endcomponent
 
     @component('components.contact')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main_style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 </head>
 <body>
