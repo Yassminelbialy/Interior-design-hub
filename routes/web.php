7e2cc666edd92b54abb0a25e575c194b0621f917
Yassminelbialy <yassminelbialy@gmail.com>
2020-06-02 19:30:05 +0200

    delete router

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/control', function () {
-    return view('Admin.sideNavBar');
-});
+
 Route::get('/ceo','AlexandrainfoController@index');
 
 
