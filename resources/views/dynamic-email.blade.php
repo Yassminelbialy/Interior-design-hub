302c80759e48ae47870a22e3f5517a7bdcbf790b
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-03 18:59:48 +0200

    edit

--- resources/views/dynamic-email.blade.php
+++ resources/views/dynamic-email.blade.php
@@ -1,3 +1,3 @@
-<h3> Hello i'm {{ $usersData['username'] }}</h3>
+<h1 style="color:red">  Hello i'm {{ $usersData['username'] }}</h1>
 <p> My phone number : {{ $usersData['phone'] }} </p>
 <p> i'm available at {{ $usersData['date'] }} you can call me :)</p>
