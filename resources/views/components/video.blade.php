3a657f59b4be0b22b42ee8f26e8e34e6e1ff8126
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-04 22:23:32 +0200

    videos

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -13,9 +13,8 @@
                         <!-- Modal -->
                         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-body">
-                                        
+                                <div class="modal-content" >
+                                    <div class="modal-body">   
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true" >&times;</span>
                                         </button>
