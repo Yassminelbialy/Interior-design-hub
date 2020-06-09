7efdeef6d8b917d3cd4701a3b38530fe6de79624
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-09 21:03:23 +0200

    Jops

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -80,6 +80,7 @@
                                     <a class="nav-link" href="/manager/contacts">Contacts</a>
                                     <a class="nav-link" href="/manager/AdminOrder">Users Orders</a>
                                     <a class="nav-link" href="/manager/fbPosts">FaceBook Posts</a>
+                                    <a class="nav-link" href="/manager/jops">Jops</a>
                                     {{-- <a class="nav-link" href="/manager/project">projects</a>
                                     <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a></nav> --}}
                             </div>
