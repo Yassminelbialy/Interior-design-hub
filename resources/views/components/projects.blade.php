a62afcbfef03c0dbef4144c46aeed03132252290
AhmedSaidShahata <ahmedsaidshehata1@gmail.com>
2020-06-11 17:59:56 +0200

    add project from database

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -1,5 +1,5 @@
  <!-- start projects -->
- <!-- <section class="projects">
+<!-- <section class="projects">
      @if(count($projects)>0)
      <h2 class="text-left">HAWN NPOEktbi:</h2>
      <div class="container-fluid">
@@ -36,50 +36,33 @@
      <h2> Last Projects</h2>
      <div class="container-fluid">
          <div class="row">
+         @forelse  ($projects as $project)
              <div class="col-md-6">
                  <div class="project_card">
                      <div class="face face1">
-                         <img src="/images/pictures_projects/8.jpg" alt="" />
+                         <img src="/projectimages/{{$project->mainImage}}" class="w-100 h-100" alt="" />
                      </div>
                      <div class="face face2">
                          <div class="content">
                              <h2>
-                                 Lorem, ipsum
+                             {{$project->title}}
                              </h2>
                              <p>
-                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-                                 atque sunt ab ipsum? Et veniam error unde repellat voluptates
-                                 maxime, harum distinctio voluptatum quas voluptatem corporis.
-                                 Quisquam molestiae repudiandae hic?
+                             {{$project->hint}}
                              </p>
-                             <a href="#">show details</a>
+                             <a class="btn btn-dark mb-1 text-light" href="{{ url('view/'.$project->id)}}">View Project</a>
 
                          </div>
                      </div>
                  </div>
              </div>
-             <div class="col-md-6">
-                 <div class="project_card">
-                     <div class="face face1">
-                         <img src="./images/pictures_projects/2.jpeg" alt="" />
-                     </div>
-                     <div class="face face2">
-                         <div class="content">
-                             <h2>
-                                 Lorem, ipsum
-                             </h2>
-                             <p>
-                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-                                 atque sunt ab ipsum? Et veniam error unde repellat voluptates
-                                 maxime, harum distinctio voluptatum quas voluptatem corporis.
-                                 Quisquam molestiae repudiandae hic?
-                             </p>
-                             <a href="#">show details</a>
 
-                         </div>
-                     </div>
-                 </div>
+             @empty
+             <div class="alert alert-info text-center" style="margin:0 auto;width:50%">
+                <h1>No Projects Yet</h1>
              </div>
+             @endforelse
+
 
              <div class="col-md-6">
                  <div class="project_card">
@@ -172,6 +155,30 @@
                      </div>
                  </div>
              </div>
+
+             <div class="col-md-6">
+                 <div class="project_card">
+                     <div class="face face1">
+                         <img src="/images/pictures_projects/4.jpeg" alt="" />
+                     </div>
+                     <div class="face face2">
+                         <div class="content">
+                             <h2>
+                                 Lorem, ipsum
+                             </h2>
+                             <p>
+                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                                 atque sunt ab ipsum? Et veniam error unde repellat voluptates
+                                 maxime, harum distinctio voluptatum quas voluptatem corporis.
+                                 Quisquam molestiae repudiandae hic?
+                             </p>
+                             <a href="#">show details</a>
+
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
 
 
          </div>
