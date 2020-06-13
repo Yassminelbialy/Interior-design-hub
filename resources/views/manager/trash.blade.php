971cf28076c9ad1a0024efb6c6005c962c581c98
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-13 02:29:16 +0200

    jop soft delete and force delete

--- resources/views/manager/trash.blade.php
+++ resources/views/manager/trash.blade.php
@@ -6,10 +6,6 @@
   <table class="table table-dark" style="background-color: rgba(0,0,0,0.5);">
 
         <thead>
-            <tr>
-                <td colspan="4"><a href="{{route('manager.trash.index')}}"><i class="fas fa-trash fa-4x" style="color: blue"></i></a></td>
-                <td colspan="4"><a href="{{route('manager.jops.create')}}"><i class="fas fa-plus fa-4x" style="color: blue"></i></a></td>
-             </tr>
           <tr>
             <th scope="col" class="text-light h3">#</th>
             <th scope="col" class="text-light h3">Title</th>
@@ -29,25 +25,59 @@
               <td>{{$jop->requirements}}</td>
               <td>{{$jop->jopType}}</td>
               <td>{{$jop->location}}</td>
+              
               <td>
-                <a href="{{route('manager.jops.show',$jop->id)}}"> 
-                  <i class="fas fa-binoculars fa-2x" style="color: green"></i>
-                </a>
-
-              </td>
-              <td> 
-                <a href="{{ route('manager.jops.edit', $jop->id) }}" > <i class="fas fa-edit fa-2x" style="color: blue"></i></a>      
+                <a href="{{ route('manager.trash.edit', $jop->id) }}"class="btn btn-success">Active</a>
               </td>
               <td>
-                {!!Form::open(['route'=>[ 'manager.jops.destroy' , $jop->id],'method'=>'delete','style'=>'    display: inline-block '])!!}
-                {{ Form::button('<i style="color:red"class="fa fa-trash fa-2x"></i>', ['type' => 'submit'] )  }}
-
-
-          {!! Form::close() !!}
-        </td>
-      </tr>
+                {!! Form::open(['route' => ['manager.trash.destroy',$jop->id] ,'method' => 'delete', 'style'=>'display:inline-block']) !!}
+                {!! Form::submit('Delete',['class'=>'btn btn-secondary']) !!}
+                {!! Form::close() !!}
+              </td>
+            </tr>
       @endforeach
     </tbody>
   </table>
 </div>
 @endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
