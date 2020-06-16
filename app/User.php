289d6b9247ff3dbe75fb2bd0e92cf8c1e856f633
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-16 20:12:12 +0200

    conflict

--- app/User.php
+++ app/User.php
@@ -39,6 +39,10 @@ class User extends Authenticatable
     public function instagram(){
         return $this->hasOne(Instagram::class, 'user_id', 'id');
     }
+    public function company()
+    {
+        return $this->hasOne('App\Company','id','company_id');
+    }
 
 
 }
