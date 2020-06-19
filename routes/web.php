e59e57aa215941882e1c95d2dc348d6e06730e19
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-19 21:03:23 +0200

    company panel edit

--- routes/web.php
+++ routes/web.php
@@ -115,15 +115,12 @@ Route::get('jops', 'Manager\JopApplicantController@index')->name('jops');
         Route::resource('AdminOrder', 'CompanyAdmin\OrderAdminController');
         Route::get('users/{users}/order', 'CompanyAdmin\OrderAdminController@updateOrder')->name('order');
         Route::resource('jopAppli', 'CompanyAdmin\JopApplicantController');
-        Route::resource('chatList', 'CompanyAdmin\ChatAdminController');
-<<<<<<< HEAD
+        Route::resource('chatList', 'CompanyAdmin\ChatAdminController');       
         Route::resource('service', 'CompanyAdmin\ServiceController');
         Route::resource('serviceTrash' ,'CompanyAdmin\ServiceTrashController');
-=======
         Route::get('/{id?}', 'UserController@indexCompany');
->>>>>>> c3c6da628bd99cabe88a5335a6a82af2bb287529
 
-}); //manager routes
+}); 
 
 
 Route::get('dddd', function () {
