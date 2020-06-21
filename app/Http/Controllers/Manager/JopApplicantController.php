a4540ea562c70cd3a473d5790964588aa6484813
ahmedHazem-iti <ahmed_hazem_refaie@hotmail.com>
2020-06-22 01:23:42 +0200

    complete job added ajax

--- app/Http/Controllers/Manager/JopApplicantController.php
+++ app/Http/Controllers/Manager/JopApplicantController.php
@@ -7,6 +7,8 @@ use App\Jop;
 use Uuid;
 use Illuminate\Http\Request;
 use App\Http\Requests\JopApplicantPost;
+use \Validator;
+use DateTime;
 
 class JopApplicantController extends Controller
 {
@@ -39,16 +41,29 @@ class JopApplicantController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(JopApplicantPost $request,$id=null)
+    public function store(Request $request,$id=null)
     {
-        $birthDate=$request->age;
-        $birthDate = explode("/", $birthDate);
-        //get age from date or birthdate
-        $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
-         ? ((date("Y") - $birthDate[2]) - 1)
-         : (date("Y") - $birthDate[2]));
+        // return response()->json(['message'=>'application form added successfully']);
 
-         $request['age']=$age+1;
+        $validator = Validator::make($request->all(), [
+            'fullName' => 'required|max:255',
+            'email' => 'required|email:rfc,dns|unique:jop_applicants',
+            'phone' => 'regex:/^01[0-2][0-9]{8}$/i',
+            'age'=>'date',
+            'urlprotofolio'=>'url',
+            "file" => "required|mimes:pdf|max:10000",
+
+             ]);
+        if ($validator->fails()) {
+            return response()->json(['erors'=>$validator->messages()->all(),'data'=>$request->all()]);
+        }
+
+        $from = new DateTime($request->age);
+        $to   = new DateTime('today');
+        $age = $from->diff($to)->y;
+
+
+         $request['age']=$age;
          if ($files = $request->file('file'))
          {
                          $uuid =Uuid::generate()->string;
@@ -65,9 +80,9 @@ class JopApplicantController extends Controller
                 JopApplicant::create($request->except(['gender','file'])        );
 
 }
+return response()->json(['message'=>'application form added successfully','s'=>$age]);
+}
 
-        return redirect('/')->with('success', 'Done');
-    }
 
     /**
      * Display the specified resource.
--- resources/views/jopApp/jops.blade.php
+++ resources/views/jopApp/jops.blade.php
@@ -72,4 +72,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<script src="{{URL::asset('js/consultationscript.js')}}"></script>
+<script src="{{URL::asset('/js/jobformscript copy.js')}}"></script>
