5bdc9524b8aeead3f2ad17323d9e10db6ab5334f
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-23 14:44:11 +0200

    Services in home

--- resources/views/components/all_services.blade.php
+++ resources/views/components/all_services.blade.php
@@ -4,7 +4,7 @@
         <h2 class="mt-5">Our Services</h2>
         <div class="services_content">
             <div class="service_box">
-                @foreach ($errors as $service )
+                @foreach ($services as $service )
                 <div class="box">
                     <div class="icon">{{$service->id}}</div>
                     <div class="content">
