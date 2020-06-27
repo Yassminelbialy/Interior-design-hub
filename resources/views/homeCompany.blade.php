2042a95699227e97e95901dd4a93ac12c0b3e8d5
Aya Ramadan <ayaramadan2019senior@gmail.com>
2020-06-27 14:18:11 +0200

    test

--- resources/views/homeCompany.blade.php
+++ resources/views/homeCompany.blade.php
@@ -6,7 +6,6 @@
 
 
 
-
 @foreach ($errors->all() as $error)
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 <strong>Holy guacamole!</strong> You should check in on some of those fields below. for {{$error}}
